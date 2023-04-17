943f974c71fcd5cadf1520aa1fbd396a0d6f91a7
Valentin-Torres-valenzuela <torresvalenzuelavalentin@gmail.com>
2023-04-17 14:53:06 -0300

    refactorizando codigo de las tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,68 +6,52 @@ const Task = ({tarea, eliminarTarea, actualizarTarea}) => {
     const [prioridad, setPrioridad] = useState(tarea.prioridad);
     const [estado, setEstado] = useState(tarea.estado);
 
-    let priorityColor = "";
-
-    if (prioridad === "Baja" ) {
-        priorityColor = "green";
-    } else if (prioridad === "Media") {
-        priorityColor = "blue";
-    } else if (prioridad === "Alta" ) {
-        priorityColor = "red";
-    }
+    const priorityColorMap = {
+        "Baja": "green",
+        "Media": "blue",
+        "Alta": "red"
+    };
     
-    let stateColor = "";
-    if (estado === "Nuevo") {
-        stateColor = "green";
-    } else if (estado === "En proceso") {
-        stateColor = "blue";
-    } else if (estado === "Finalizado") {
-        stateColor = "red";
-    }
-
-    const actualizarPrioridad = () => {
-        switch (prioridad) {
-            case "Baja":
-                setPrioridad("Media");
-                actualizarTarea(tarea.id, { prioridad: "Media" });
-                break;
-            case "Media":
-                setPrioridad("Alta");
-                actualizarTarea(tarea.id, { prioridad: "Alta" });
-                break;
-            case "Alta":
-                setPrioridad("Baja");
-                actualizarTarea(tarea.id, { prioridad: "Baja" });
-                break;
-            default:
-        }
-    }
+    const stateColorMap = {
+        "Nuevo": "green",
+        "En proceso": "blue",
+        "Finalizado": "red"
+    };
     
-
-    const actualizarEstado = () => {
-    switch (estado) {
-        case "Nuevo":
-            setEstado("En proceso");
-            actualizarTarea(tarea.id, { estado: "En proceso" });
-            break;
-        case "En proceso":
-            setEstado("Finalizado");
-            actualizarTarea(tarea.id, { estado: "Finalizado" });
-            break;
-        case "Finalizado":
-            setEstado("Nuevo");
-            actualizarTarea(tarea.id, { estado: "Nuevo" });
-            break;
-        default:
-    }
-}
+    const priorityColor = priorityColorMap[prioridad];
+    const stateColor = stateColorMap[estado];
+    
+    const priorityUpdateMap = {
+        "Baja": { newPriority: "Media" },
+        "Media": { newPriority: "Alta" },
+        "Alta": { newPriority: "Baja" }
+    };
+    
+    const stateUpdateMap = {
+        "Nuevo": { newState: "En proceso" },
+        "En proceso": { newState: "Finalizado" },
+        "Finalizado": { newState: "Nuevo" }
+    };
+    
+    const updatePriority = () => {
+        const { newPriority } = priorityUpdateMap[prioridad];
+        setPrioridad(newPriority);
+        actualizarTarea(tarea.id, { prioridad: newPriority });
+    };
+    
+    const updateState = () => {
+        const { newState } = stateUpdateMap[estado];
+        setEstado(newState);
+        actualizarTarea(tarea.id, { estado: newState });
+    };
+    
 
 
     return (
         <div className="task">
             <div className="task__options__container">
-                <p>Prioridad: <span className="priority__span" style={{ background: priorityColor }} onClick={actualizarPrioridad}>{prioridad}</span></p>
-                <p>Estado: <span className="priority__span" style={{ background: stateColor }} onClick={actualizarEstado}>{estado}</span></p>
+                <p>Prioridad: <span className="priority__span" style={{ background: priorityColor }} onClick={updatePriority}>{prioridad}</span></p>
+                <p>Estado: <span className="priority__span" style={{ background: stateColor }} onClick={updateState}>{estado}</span></p>
             </div>
             <div className="task__title">
                 <p>Titulo: <h3>{tarea.titulo}</h3></p>
